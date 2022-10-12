6587e9cef1da8275307485a4006d763ee21353cf
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-12 23:10:05 +0530

    cars list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CarListComponent } from './car-list/car-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CarListComponent
   ],
   imports: [
     BrowserModule,
